dbe89c4479299225553e7376f1408957053725a5
Patrick Vilhena & John Olmsted <pair+nifengle.qsymmachus@devbootcamp.com>
2014-01-24 12:36:21 -0800

    began work on ToDo and ToDoController

--- source/ar_todos/todo.rb
+++ source/ar_todos/todo.rb
@@ -1,3 +1,30 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+#-----VIEWER-----
+
+class ToDo
+  def self.run(command, *argument)
+    ToDoController.process_command(command, argument)
+    puts "ok"
+  end
+end
+
+
+#-----CONTROLLER-----
+
+class ToDoController
+  def self.process_command(command, *argument)
+    self.send(command, *argument)
+  end
+
+  def self.list
+    puts "Hello, world!"
+  end
+end
+
+
+#-----ARGV-----
+puts "before"
+command = ARGV.shift
+argument = ARGV.join(" ") unless ARGV.empty?
+ToDo.run(command, argument)
