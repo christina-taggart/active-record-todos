6ff6575300f115b475d4bc81dbd4f7d5d8804ff0
mh120888 <matthew.alan.higgins@gmail.com>
2014-01-24 23:50:41 -0800

    Wrote methods for #add, #delete, and #get_list in list.rb

--- source/ar_todos/app/models/list.rb
+++ source/ar_todos/app/models/list.rb
@@ -4,11 +4,27 @@ class List < ActiveRecord::Base
   has_many :tasks
 
   def self.get_list(id)
-    List.find(id).tasks.to_a.map(&:description)
+    List.tasks_on_this_list(id).map(&:description)
   end
+
+  def self.add_task(list_id, task_description)
+  	Task.create(description: task_description, done: "0", list_id: list_id)
+  end
+
+  def self.delete_task(list_id, task_position)
+  	tasks = List.tasks_on_this_list(list_id)
+  	tasks[task_position-1].destroy
+  end
+
+  def self.tasks_on_this_list(list_id) # returns an array of task objects belonging to a specified list
+  	List.find(list_id).tasks.to_a
+  end
+
+  def self.complete_task(list_id, task_position)
+  	tasks = List.tasks_on_this_list(list_id)
+  	tasks[task_position-1][:complete] = "1"
+  end
+
 end
 
-# List.show_list(1)
-# puts
-# p List.show_list(2)
 
