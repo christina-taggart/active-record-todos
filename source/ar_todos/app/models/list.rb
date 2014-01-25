ca5f84e941b577e480a07c49c47ea4781e9dd4ab
mh120888 <matthew.alan.higgins@gmail.com>
2014-01-25 00:37:38 -0800

    Update todo.rb, can now take command line arguments to CRUD tasks/lists

--- source/ar_todos/app/models/list.rb
+++ source/ar_todos/app/models/list.rb
@@ -7,13 +7,13 @@ class List < ActiveRecord::Base
     List.tasks_on_this_list(id).map(&:description)
   end
 
-  def self.add_task(list_id, task_description) # adds a task to a specified list
+  def self.add(list_id, task_description) # adds a task to a specified list
   	Task.create(description: task_description, done: "0", list_id: list_id)
   end
 
   def self.delete_task(list_id, task_position) # deletes a task from a specified list
   	tasks = List.tasks_on_this_list(list_id)
-  	tasks[task_position-1].destroy
+  	tasks[task_position.to_i-1].destroy
   end
 
   def self.tasks_on_this_list(list_id) # returns an array of task objects belonging to a specified list
@@ -22,7 +22,7 @@ class List < ActiveRecord::Base
 
   def self.complete_task(list_id, task_position) # marks a specified task on a specified list as complete (i.e. "1")
   	tasks = List.tasks_on_this_list(list_id)
-  	tasks[task_position-1].update_attribute(:done, "1")
+  	tasks[task_position.to_i-1].update_attribute(:done, "1")
   end
 
   # stuff for controller/view, not model
@@ -35,10 +35,9 @@ class List < ActiveRecord::Base
   end
 
   def self.delete(list_id, task_position) # deletes a given task from the list and displays desired message to user
-    deleted_item = List.delete_task(list_id, task_position)
+    deleted_item = List.delete_task(list_id, task_position.to_i)
     puts "You just deleted '#{deleted_item[:description]}'' from your list."
   end
 
 end
-
 
--- source/ar_todos/todo.rb
+++ source/ar_todos/todo.rb
@@ -1,3 +1,12 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+
+if ARGV.length == 3
+	List.send ARGV[0].to_sym, ARGV[1], ARGV[2]
+elsif ARGV.length == 2
+	List.send ARGV[0].to_sym, ARGV[1]
+else 
+	puts "Wrong number of arguments...try again"
+end
